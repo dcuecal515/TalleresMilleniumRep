6769b4d5d7a1eedf1b54670743dc374778fb998f
dcuecal515 <dcuecal515@g.educaand.es>
2025-04-14 18:20:07 +0200

    Subida de cambios leves pero necesarios

--- Web/Front/TalleresMillenium/src/app/landing-page/landing-page.component.ts
+++ Web/Front/TalleresMillenium/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  constructor(private router:Router){
+    
+  }
 
+  irAInicioSesion(){
+    this.router.navigateByUrl("/inicio-sesion");
+  }
+
+  irASobreNosotros(){
+    this.router.navigateByUrl("/sobre-nosotros");
+  }
 }
