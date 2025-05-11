e6b2131af4d0265d9c9df61e3d51d0d9c12d12de
dcuecal515 <dcuecal515@g.educaand.es>
2025-05-11 11:46:43 +0200

    Subida de mejoras pequeñas

--- Web/Front/TalleresMillenium/src/app/app.routes.ts
+++ Web/Front/TalleresMillenium/src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
 import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
-import { ChatComponent } from './component/chat/chat.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'inicio-sesion',component:InicioSesionComponent},
-    {path:'sobre-nosotros',component:SobreNosotrosComponent},
-    {path:'chat',component:ChatComponent}
+    {path:'sobre-nosotros',component:SobreNosotrosComponent}
 ];
