8f0c64ed3bcdd63b034f5c89d4f0becbfe393900
dcuecal515 <dcuecal515@g.educaand.es>
2025-05-21 20:09:55 +0200

    Perfil front casi hecho

--- Web/Front/TalleresMillenium/src/app/pages/perfil/perfil.component.ts
+++ Web/Front/TalleresMillenium/src/app/pages/perfil/perfil.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
 import { User } from '../../models/user';
 import { FullUser } from '../../models/FullUser';
+import { Coche } from '../../models/Coche';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-perfil',
@@ -13,7 +15,7 @@ import { FullUser } from '../../models/FullUser';
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
-  constructor(private authService:AuthService, private router:Router){
+  constructor(private authService:AuthService, public router:Router){
     if(localStorage.getItem("token")){
       this.decoded=jwtDecode(localStorage.getItem("token"));
     }else if(sessionStorage.getItem("token")){
@@ -24,12 +26,41 @@ export class PerfilComponent {
 
   decoded:User
   user:FullUser | null = null
+  matricula_actual:string = ""
+  coche_actual:Coche | null = null
 
   async getUser(){
     const result = await this.authService.getFullUser(this.decoded.id)
 
     this.user = result.data;
+    this.user.imagen = environment.images+this.user.imagen
+    this.user.coches.forEach(coche => {
+      coche.imagen = environment.images+coche.imagen
+    });
+    console.log(this.user.imagen)
+    this.matricula_actual = this.user.coches[0].matricula
+    this.coche_actual = this.user.coches[0]
 
     console.log("Usuario: ",this.user)
+  }
+
+  seleccionar_coche(){
+
+  }
+
+  anadir_coche(){
+
+  }
+
+  cambiar_nombre(){
+
+  }
+
+  cambiar_email(){
+
+  }
+
+  cambiar_contrasena(){
+    
   }
 }
