28eadd28c118f0a4d3e9075cca07869c751fdae0
dcuecal515 <dcuecal515@g.educaand.es>
2025-06-07 16:30:47 +0200

    Subida de mejora del alert dialog y hecho la parte de aditar y eliminar producto

--- Aplicacion/app/src/main/java/com/example/talleresmileniumapp/Data/Routes.kt
+++ Aplicacion/app/src/main/java/com/example/talleresmileniumapp/Data/Routes.kt
@@ -6,6 +6,7 @@ sealed class Routes (val route : String) {
     data object ProductosYServicios : Routes("ProductosYServicios");
     data object Usuarios : Routes("Usuarios");
     data object Reservas : Routes("Reservas");
+    data object EditProduct : Routes("EditProduct")
     data object TasksManager : Routes("TasksManager");
 
 }
