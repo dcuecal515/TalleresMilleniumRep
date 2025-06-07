9a504f3e46b26131c9f71078bda35c9c4183037c
FranSiles <francisco.siles.fp@iescampanillas.com>
2025-06-07 16:35:53 +0200

    subida de cmbios que puede hacer el administrador a un usuario en la aplicación movil

--- Aplicacion/app/src/main/java/com/example/talleresmileniumapp/Repositories/UserRepository.kt
+++ Aplicacion/app/src/main/java/com/example/talleresmileniumapp/Repositories/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.example.talleresmileniumapp.Repositories
 
 import com.example.talleresmileniumapp.Models.RetrofitApiInstance.userService
+import com.example.talleresmileniumapp.Models.User.ChangeRolRequest
 import com.example.talleresmileniumapp.Models.User.UserResponse
 
 class UserRepository {
@@ -8,5 +9,12 @@ class UserRepository {
         val response= userService.getallUser("Bearer $token")
 
         return response.body()
+    }
+    suspend fun putadmin(token: String,id: String,rol: String){
+        val request = ChangeRolRequest(id = id, rol = rol)
+        userService.putadmin("Bearer $token",request)
+    }
+    suspend fun deleteuser(token: String,id: String){
+        userService.deleteuser("Bearer $token",id)
     }
 }
